77e8cbb546345e4421cdce34e6b54458dc2d72a0
JosephTrauth <Josepht97@gmail.com>
2017-02-22 09:31:03 -0500

    added ablity to see and track purchases

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   #support stripe payments throught charges
   resources :charges  
 
+  # route for showing purchases
+  
+
+  resources :purchases, only: [:show]
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
